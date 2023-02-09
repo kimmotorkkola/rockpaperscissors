5a6871529d956e3e881b8ab4180052c251a6fecb
kimmotorkkola <118106081+kimmotorkkola@users.noreply.github.com>
2023-02-09 16:10:03 +0200

    Added buttons to the game

--- rps.js
+++ rps.js
@@ -6,6 +6,7 @@ function getComputerChoice(){
     return values[random];
 }
 
+
 //function to get a round of rock, paper, scissors
 function playRound(playerSelection, computerSelection){
 
@@ -16,35 +17,45 @@ function playRound(playerSelection, computerSelection){
     } else if(playerSelection === "rock" && computerSelection === "paper" ||
             playerSelection === "scissors" && computerSelection === "rock" ||
             playerSelection === "paper" && computerSelection === "scissors"){
-        computerScore++;
+        //computerScore++;
         return `You Lost! ${computerSelection} beats ${playerSelection}`}
 
      else if(playerSelection === "rock" && computerSelection === "scissors" ||
             playerSelection === "scissors" && computerSelection === "paper" ||
             playerSelection === "paper" && computerSelection === "rock"){
-        userScore++;
+        //userScore++;
         return `You Won! ${playerSelection} beats ${computerSelection}`}
-            }
+            };
 
+//finding a playerSelection and triggering the game
+const choiceBtns = document.querySelectorAll(".choiceBtn");
 
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
+    const playerSelection = button.textContent;
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+    
+}))
+
+//let userScore = parseInt(0);
+//let computerScore = parseInt(0);
 
 //Loop to play 5 rounds and declare a winner
-for(let i = 0; i < 5; i++){
+/*for(let i = 0; i < 5; i++){
 let playerSelection = prompt("Choose rock, paper or scissors");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection.toLowerCase(), computerSelection));
 console.log("Your score = " + userScore);
 console.log("Computer's score = " + computerScore);
-}
+}*/
+
 
 //Print out a winner of the best of 5
-if (computerScore > userScore){
+/*if (computerScore > userScore){
     console.log("Computer wins the best of 5!");
 } else if(userScore > computerScore){
     console.log("You win the best of 5!");
 }else{
     console.log("Best of 5 is a tie!");
-}
+}*/
