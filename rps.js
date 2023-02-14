5cd06c7efc8cfd6edfb879af1333f0499f253407
kimmotorkkola <118106081+kimmotorkkola@users.noreply.github.com>
2023-02-14 15:28:06 +0200

    Print game results as DOM nodes

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+const results = document.querySelector('#results');
+const winner = document.querySelector('#winner');
 
 //function to get a random choice for the computer
 function getComputerChoice(){
@@ -17,13 +19,13 @@ function playRound(playerSelection, computerSelection){
     } else if(playerSelection === "rock" && computerSelection === "paper" ||
             playerSelection === "scissors" && computerSelection === "rock" ||
             playerSelection === "paper" && computerSelection === "scissors"){
-        //computerScore++;
+        computerScore++;
         return `You Lost! ${computerSelection} beats ${playerSelection}`}
 
      else if(playerSelection === "rock" && computerSelection === "scissors" ||
             playerSelection === "scissors" && computerSelection === "paper" ||
             playerSelection === "paper" && computerSelection === "rock"){
-        //userScore++;
+        userScore++;
         return `You Won! ${playerSelection} beats ${computerSelection}`}
             };
 
@@ -31,15 +33,42 @@ function playRound(playerSelection, computerSelection){
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
-
+    if(results.hasChildNodes()){
+        results.removeChild(results.children[0]);
+    } 
+    if(winner.hasChildNodes()){
+        winner.removeChild(winner.children[0]);
+    }
     const playerSelection = button.textContent;
     const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection.toLowerCase(), computerSelection));
-    
-}))
+   // console.log(playRound(playerSelection.toLowerCase(), computerSelection));
 
-//let userScore = parseInt(0);
-//let computerScore = parseInt(0);
+    const newResult = document.createElement('div');
+    newResult.classList.add('newResult');
+    
+    newResult.innerHTML = `<h2>${playRound(playerSelection.toLowerCase(), computerSelection)}</h2>
+    <p> Your score = ${userScore} </p>
+    <p> Computer's score = ${computerScore}</p>`; 
+    results.appendChild(newResult);
+
+    const newWinner = document.createElement("h1");
+    newWinner.classList.add('newWinner');
+
+    if (computerScore === 5){
+        newWinner.innerHTML = "Computer wins the best to 5!";
+        winner.appendChild(newWinner);
+    } else if(userScore === 5){
+        newWinner.innerHTML = "You win the best to 5!";
+        winner.appendChild(newWinner);
+    }else{
+        newWinner.innerHTML = "Who will win Best to 5?";
+        winner.appendChild(newWinner);
+    }
+    
+}));
+
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
 
 //Loop to play 5 rounds and declare a winner
 /*for(let i = 0; i < 5; i++){
@@ -51,11 +80,11 @@ console.log("Computer's score = " + computerScore);
 }*/
 
 
-//Print out a winner of the best of 5
-/*if (computerScore > userScore){
-    console.log("Computer wins the best of 5!");
-} else if(userScore > computerScore){
-    console.log("You win the best of 5!");
+//Print out a winner of the best to 5
+/*if (computerScore === 5){
+    console.log("Computer wins the best to 5!");
+} else if(userScore === 5){
+    console.log("You win the best to 5!");
 }else{
-    console.log("Best of 5 is a tie!");
+    console.log("Who will win Best to 5?");
 }*/
